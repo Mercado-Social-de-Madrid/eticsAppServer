d30c22bd2ac46adbb74a0788c28ee1b7cb6fb4f8
Joseba S <jjoseba@gmail.com>
2017-09-27 13:35:59 +0200

    Added wallet model

--- currency/models/__init__.py
+++ currency/models/__init__.py
@@ -1,2 +1,3 @@
 from category import Category
 from entity import Entity
+from wallet import Wallet
---
+++ currency/models/wallet.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import uuid
+
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Wallet(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User)
+    balance = models.FloatField(default=0, verbose_name='Saldo actual')
+    last_transaction = models.DateTimeField(blank=True, null=True, verbose_name='Última transacción')
+
+    class Meta:
+        verbose_name = 'Monedero'
+        verbose_name_plural = 'Monederos'
+        ordering = ['user']
+
+    def __unicode__(self):
+        return self.name
