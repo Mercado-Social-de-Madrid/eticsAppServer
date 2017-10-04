e6c6b88f56d8ce4b26866d16539709304aa7f518
Joseba S <jjoseba@gmail.com>
2017-10-04 10:12:34 +0200

    Added offers model to the Django admin page

--- currency/admin.py
+++ currency/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from currency.models import Entity, Category, Wallet
+from currency.models import Entity, Category, Wallet, Offer
 
 admin.site.register(Entity)
 admin.site.register(Category)
 admin.site.register(Wallet)
+
+class OffersAdmin(admin.ModelAdmin):
+    list_display = ('entity', 'title', 'active', 'published_date', 'begin_date', 'end_date')
+    ordering = '-published_date',
+   
+admin.site.register(Offer, OffersAdmin)
