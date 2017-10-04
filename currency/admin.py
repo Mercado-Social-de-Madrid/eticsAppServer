fb59a9c09c0f52b786621e105f0bd7acf341a2f8
Joseba S <jjoseba@gmail.com>
2017-10-04 10:17:15 +0200

    Updated how to show offer dates in Django admin

--- currency/admin.py
+++ currency/admin.py
@@ -8,7 +8,20 @@ admin.site.register(Category)
 admin.site.register(Wallet)
 
 class OffersAdmin(admin.ModelAdmin):
-    list_display = ('entity', 'title', 'active', 'published_date', 'begin_date', 'end_date')
+
+    def pub_date(self, obj):
+        return obj.published_date.strftime("%d/%M/%Y")
+
+    def begin_date_format(self, obj):
+        return obj.begin_date.strftime("%d/%M/%Y")
+
+    def end_date_format(self, obj):
+        return obj.end_date.strftime("%d/%M/%Y")
+
+    list_display = ('entity', 'title', 'active', 'pub_date', 'begin_date_format', 'end_date_format')
     ordering = '-published_date',
-   
+    pub_date.short_description = 'Publicado'
+    begin_date_format.short_description = 'Fecha inicio'
+    end_date_format.short_description = 'Fecha fin'
+
 admin.site.register(Offer, OffersAdmin)
