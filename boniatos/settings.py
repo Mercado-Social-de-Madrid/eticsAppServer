97a9d24b140b504c006592772db989d98f7ecdb9
Joseba S <jjoseba@gmail.com>
2018-01-18 13:33:40 +0100

    Added base template and frontend libraries

--- boniatos/settings.py
+++ boniatos/settings.py
@@ -109,6 +109,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = ROOT_DIR + '/static'
 MEDIA_ROOT = ROOT_DIR + '/media'
 MEDIA_URL = '/media/'
