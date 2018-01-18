80a06663f6498ada7f8cbda78a67b6cc1c1ae233
Joseba S <jjoseba@gmail.com>
2018-01-18 19:56:46 +0100

    Added `django-cleanup` for image management

--- boniatos/settings.py
+++ boniatos/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'currency',
     'tastypie',
     'imagekit',
+    'django_cleanup',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
