49d2899fabceaca0881be0907d77b8b984e7514b
Joseba S <jjoseba@gmail.com>
2018-01-22 16:58:17 +0100

    Finished dashboard

--- currency/views/main.py
+++ currency/views/main.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
@@ -8,7 +9,12 @@ def index(request):
 
 @login_required
 def profile(request):
+    UserModel = get_user_model()
+    type, entity = UserModel.get_related_entity(request.user)
 
     params = {}
+    if type != 'none':
+        params['type'] = type
+        params['entity'] = entity
 
     return render(request, 'profile/index.html', params)
