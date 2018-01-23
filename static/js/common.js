e628e4fc4a4bd1b0617db6d4b70009a90e8e322e
Joseba S <jjoseba@gmail.com>
2018-01-23 13:13:31 +0100

    Extracted common gallery functionality to a component

--- static/js/common.js
+++ static/js/common.js
@@ -26,4 +26,16 @@ $(function(){
         }
     });
 
+    $('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		}
+	});
+
+
 });
