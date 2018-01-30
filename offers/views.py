467af7f20ad6c42d42ba3de1c583f7eecee927fd
Joseba S <jjoseba@gmail.com>
2018-01-30 11:32:46 +0100

    Added offers list linked from menus

--- offers/views.py
+++ offers/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
@@ -14,19 +15,28 @@ from offers.models import Offer
 
 
 @login_required
-def entity_offers(request, pk):
-    entity = get_object_or_404(Entity, pk=pk)
+def user_offers(request):
+    type, entity = get_user_model().get_related_entity(request.user)
+    if type == 'entity':
+        return redirect('entity_offers', entity_pk= entity.pk)
+
+
+@login_required
+def entity_offers(request, entity_pk):
+    entity = get_object_or_404(Entity, pk=entity_pk)
+    is_owner = request.user.is_authenticated and (request.user == entity.user)
 
     current_offers = Offer.objects.current().filter(entity=entity)
     print current_offers
     return render(request, 'offers/entity_list.html', {
         'entity': entity,
-        'current_offers': current_offers
+        'current_offers': current_offers,
+        'is_offers_owner': is_owner
     })
 
 @login_required
-def add_offer(request, pk):
-    entity = get_object_or_404(Entity, pk=pk)
+def add_offer(request, entity_pk):
+    entity = get_object_or_404(Entity, pk=entity_pk)
     can_edit = request.user.is_superuser or request.user == entity.owner
 
     if not can_edit:
