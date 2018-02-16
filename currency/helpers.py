52595387b86dac4a92cb26ca66788d29f0206ca5
Joseba S <jjoseba@gmail.com>
2018-02-16 14:19:21 +0100

    Permissions check for admin actions

--- currency/helpers.py
+++ currency/helpers.py
@@ -84,3 +84,20 @@ def notify_user(user, data, title=None, message=None, silent=True):
 
     result = device.send_message(title, message, data=data)
     print result
+
+
+from django.contrib.admin.views.decorators import user_passes_test
+
+def superuser_required(view_func=None, login_url='dashboard'):
+    """
+    Decorator for views that checks that the user is logged in and is a
+    superuser, redirecting to the login page if necessary.
+    """
+    actual_decorator = user_passes_test(
+        lambda u: u.is_active and u.is_superuser,
+        login_url=login_url,
+        redirect_field_name='permission'
+    )
+    if view_func:
+        return actual_decorator(view_func)
+    return actual_decorator
