6027bc95ed70062a2d172a90b39763e7ad1a1f3b
Joseba S <jjoseba@gmail.com>
2018-02-23 21:07:45 +0100

    Added admin interface for news

--- news/admin.py
+++ news/admin.py
@@ -4,3 +4,18 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from news.models import News
+
+
+class NewsAdmin(admin.ModelAdmin):
+
+    def pub_date(self, obj):
+        return obj.published_date.strftime("%d/%M/%Y")
+
+
+    list_display = ('pub_date', 'title')
+    ordering = '-published_date',
+    pub_date.short_description = 'Publicada'
+
+
+admin.site.register(News, NewsAdmin)
