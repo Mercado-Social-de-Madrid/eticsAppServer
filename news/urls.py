2e3859b47aaba31769684874f190d463e1ad0101
Joseba S <jjoseba@gmail.com>
2018-02-26 11:32:08 +0100

    Modified template structure

---
+++ news/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+
+    url(r'^news$', views.news_list, name='news_list'),
+#    url(r'^news/add$', views.add_news, name='add_news'),
+ #   url(r'^news/(?P<pk>[0-9a-f-]+)$', views.news_detail, name='entity_offers'),
+  #  url(r'^news/(?P<pk>[0-9a-f-]+)/edit$', views.news_edit, name='news_edit'),
+
+]
