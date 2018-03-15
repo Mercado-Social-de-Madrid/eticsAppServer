ca47459807bb9866a4815a53d37c1b3080421bbf
Joseba S <jjoseba@gmail.com>
2018-03-15 17:14:37 +0100

    Show message when wallet has not enough balance

--- wallets/views.py
+++ wallets/views.py
@@ -44,27 +44,28 @@ def payment_detail(request, pk):
         messages.add_message(request, messages.ERROR, 'No tienes permisos para ver este pago')
         return redirect('entity_detail', pk=payment.pk )
 
+    params = { 'payment': payment }
+
     if request.method == "POST":
         action = request.POST.get("action", "")
 
         if action == 'accept':
-            payment.accept_payment()
-            return redirect('pending_payments')
+            try:
+                payment.accept_payment()
+                return redirect('pending_payments')
+            except Wallet.NotEnoughBalance:
+                params['notenoughbalance'] = True
 
         if action == 'cancel':
             payment.cancel_payment()
             return redirect('pending_payments')
 
-    else:
-        sender_type, sender = payment.sender.get_related_entity()
-        receiver_type, entity = payment.receiver.get_related_entity()
+    sender_type, sender = payment.sender.get_related_entity()
+    receiver_type, entity = payment.receiver.get_related_entity()
+    params['sender'] = sender
+    params['bonus'] = entity.bonus(payment.total_amount, sender_type)
 
-        bonus = entity.bonus(payment.total_amount, sender_type)
-        return render(request, 'wallets/payment_detail.html', {
-            'payment': payment,
-            'bonus': bonus,
-            'sender': sender
-        })
+    return render(request, 'wallets/payment_detail.html', params)
 
 
 @login_required
