511dde0ab2d5ef008a3ec53cd228c2f255b5e852
Joseba S <jjoseba@gmail.com>
2018-03-21 18:52:18 +0100

    Dont print email message on log

--- helpers/mailing.py
+++ helpers/mailing.py
@@ -7,9 +7,6 @@ def send_template_email(title, destination, template_name, template_params):
     msg_plain = render_to_string('email/%s.txt' % template_name, template_params)
     msg_html = render_to_string('email/%s.html' % template_name, template_params)
 
-    print msg_html
-    print msg_plain
-
     send_mail(
         title,
         msg_plain,
