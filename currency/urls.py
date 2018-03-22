5404fb3f5ee7e36f0f3f985eb165a62cc231d16b
Joseba S <jjoseba@gmail.com>
2018-03-22 14:00:44 +0100

    Added password reset functionality

--- currency/urls.py
+++ currency/urls.py
@@ -8,6 +8,11 @@ urlpatterns = [
 
     url(r'^login/$', auth_views.login, {'redirect_authenticated_user': True }, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 
     url(r'^entity/$', views.user_entity, name='user_entity'),
     url(r'^entity/add$', views.add_entity, name='add_entity'),
