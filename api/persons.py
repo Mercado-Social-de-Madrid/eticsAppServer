af38189701d2f361b5a2305182daa7e2c7799892
Joseba S <jjoseba@gmail.com>
2019-03-19 17:49:28 +0100

    Don't update profile image from API endpoint

--- api/persons.py
+++ api/persons.py
@@ -28,3 +28,6 @@ class PersonsResource(ModelResource):
             bundle.data['profile_thumbnail'] = bundle.obj.profile_thumbnail.url
         return bundle
 
+    def hydrate(self, bundle):
+        if bundle.obj.profile_image:
+            del bundle.obj.profile_image
