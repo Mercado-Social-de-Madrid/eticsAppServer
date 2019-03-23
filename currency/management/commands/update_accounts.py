b12761f54bea439e6d4961a10b08cc2ff1d05ce1
Joseba S <jjoseba@gmail.com>
2019-03-23 21:47:20 +0100

    Updated service to update account statuses

--- currency/management/commands/update_accounts.py
+++ currency/management/commands/update_accounts.py
@@ -18,43 +18,44 @@ class Command(BaseCommand):
             help='Calculate summary tables from the beginning, not just the last ones',
         )
 
-    def compose_url(self, base_url, account_pk):
-        return '{}api/v1/account/{}'.format(base_url, account_pk)
+    def compose_url(self, base_url, account_pk, is_guest=False):
+        return ('{}api/v1/' + ('guest' if is_guest else 'account') + '/{}').format(base_url, account_pk)
 
     def handle(self, *args, **options):
-
-        cities = City.objects.all()
         users = User.objects.filter(wallet__isnull=False).distinct()
-
         for user in users:
+
             account_pk = None
+            is_guest = False
             type, account = user.get_related_entity()
             if type == 'entity':
                 account_pk = account.cif
             elif type == 'person':
                 account_pk = account.nif
+                is_guest = account.is_guest_account
 
             if not account_pk:
                 continue
 
-            print '---------'
-
-            print account
-
             if account.city.server_base_url:
-                api_url = self.compose_url(account.city.server_base_url, account_pk)
+
+                api_url = self.compose_url(account.city.server_base_url, account_pk, is_guest)
                 try:
                     r = requests.get(api_url)
 
                     if r.ok:
                         body = r.json()
                         user.is_active = body['is_active']
-                        print user.is_active
                         user.save()
-                    else:
-                        print 'User not found!'
 
-                except Exception:
+                        if is_guest:
+                            account.expiration_date = body['expiration_date']
+                            account.save()
+                        print unicode('User {} updated!').format(account)
+                    else:
+                        print unicode('User {} not found!').format(account)
+
+                except Exception as e:
                     pass
 
         #if options['fromstart']:
