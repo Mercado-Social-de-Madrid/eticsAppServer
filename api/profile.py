2716c84674e604744fdb5f0e69b6698f95d2320e
Joseba S <jjoseba@gmail.com>
2019-03-29 11:08:49 +0100

    Updated invite endpoint to work both in entities and persons

--- api/profile.py
+++ api/profile.py
@@ -12,68 +12,7 @@ from currency.models import Entity, Person
 from wallets.models import Wallet
 
 
-class EntityResource(ModelResource):
-
-    class Meta:
-        queryset = Entity.objects.all()
-        include_resource_uri = False
-        always_return_data = True
-        list_allowed_methods = ['get', 'put']
-        resource_name = 'entity'
-        excludes = ['user']
-
-        authentication = ApiKeyAuthentication()  # Endpoint based on ApiKey auth
-        authorization = Authorization()
-
-    def dispatch_list(self, request, **kwargs):
-        return self.dispatch_detail(request, **kwargs)
-
-    def put_detail(self, request, **kwargs):
-        return self.patch_detail(request, **kwargs)
-
-    def obj_get(self, bundle, **kwargs):
-        try:
-            entity = Entity.objects.get(user=bundle.request.user)
-        except Entity.DoesNotExist:
-            raise NotFound("User has no associated entity")
-
-        return entity
-
-    def hydrate(self, bundle):
-        if bundle.request.user:
-            bundle.obj.user = bundle.request.user
-        return bundle
-
-
-class PersonResource(ModelResource):
-    fav_entities = fields.ManyToManyField(EntityResource, 'fav_entities', full=False)
-
-    class Meta:
-        queryset = Person.objects.all()
-        include_resource_uri = False
-        always_return_data = True
-        list_allowed_methods = ['get', 'put']
-        resource_name = 'profile'
-        excludes = ['id']
-
-        authentication = ApiKeyAuthentication()  # Endpoint based on ApiKey auth
-        authorization = Authorization()
-
-    def dispatch_list(self, request, **kwargs):
-        return self.dispatch_detail(request, **kwargs)
-
-    def put_detail(self, request, **kwargs):
-        return self.patch_detail(request, **kwargs)
-
-    def obj_get(self, bundle, **kwargs):
-        try:
-            person = Person.objects.get(user=bundle.request.user)
-        except Person.DoesNotExist:
-            raise NotFound("User has no associated person")
-
-        return person
-
-
+class InviteResource(ModelResource):
     def prepend_urls(self):
         return [
             url(r"^invite/$", self.wrap_view('invite'), name='api_invite'),
@@ -112,6 +51,69 @@ class PersonResource(ModelResource):
                 return response
         else:
             return self.create_response(request, {'success': False}, HttpBadRequest)
+
+
+
+class EntityResource(InviteResource):
+
+    class Meta:
+        queryset = Entity.objects.all()
+        include_resource_uri = False
+        always_return_data = True
+        list_allowed_methods = ['get', 'put']
+        resource_name = 'entity'
+        excludes = ['user']
+
+        authentication = ApiKeyAuthentication()  # Endpoint based on ApiKey auth
+        authorization = Authorization()
+
+    def dispatch_list(self, request, **kwargs):
+        return self.dispatch_detail(request, **kwargs)
+
+    def put_detail(self, request, **kwargs):
+        return self.patch_detail(request, **kwargs)
+
+    def obj_get(self, bundle, **kwargs):
+        try:
+            entity = Entity.objects.get(user=bundle.request.user)
+        except Entity.DoesNotExist:
+            raise NotFound("User has no associated entity")
+
+        return entity
+
+    def hydrate(self, bundle):
+        if bundle.request.user:
+            bundle.obj.user = bundle.request.user
+        return bundle
+
+
+class PersonResource(InviteResource):
+    fav_entities = fields.ManyToManyField(EntityResource, 'fav_entities', full=False)
+
+    class Meta:
+        queryset = Person.objects.all()
+        include_resource_uri = False
+        always_return_data = True
+        list_allowed_methods = ['get', 'put']
+        resource_name = 'profile'
+        excludes = ['id']
+
+        authentication = ApiKeyAuthentication()  # Endpoint based on ApiKey auth
+        authorization = Authorization()
+
+    def dispatch_list(self, request, **kwargs):
+        return self.dispatch_detail(request, **kwargs)
+
+    def put_detail(self, request, **kwargs):
+        return self.patch_detail(request, **kwargs)
+
+    def obj_get(self, bundle, **kwargs):
+        try:
+            person = Person.objects.get(user=bundle.request.user)
+        except Person.DoesNotExist:
+            raise NotFound("User has no associated person")
+
+        return person
 
 
     def dehydrate(self, bundle):
