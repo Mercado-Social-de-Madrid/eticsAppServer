9c8c03a8e91732adf56eb75413aa0a64ce900097
Joseba S <jjoseba@gmail.com>
2023-04-27 12:49:26 +0200

    Added entity info in balance page

--- currency/views/balance.py
+++ currency/views/balance.py
@@ -6,6 +6,8 @@ from currency.models import Entity
 
 class BalanceDetail(DetailView):
     template_name = 'balance/detail.html'
+    context_object_name = 'entity'
 
     def get_object(self, queryset=None):
-        return get_object_or_404(Entity, member_id=self.kwargs['member_id'])
+        return Entity.objects.filter(member_id=self.kwargs['member_id']).first()
+
